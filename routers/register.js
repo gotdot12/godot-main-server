ad528ffccf8e2b84f6fa77cc72755be3b7dba26d
gotdot12 <129498729+gotdot12@users.noreply.github.com>
2023-05-24 17:02:50 +0530

    Update register.js

--- routers/register.js
+++ routers/register.js
@@ -2,15 +2,16 @@ const express = require("express");
 const router = express.Router();
 const user = require("../models/user.model");
 const shortid = require('shortid');
+const mailer = require("nodemailer");
 
-
+const senderMail = "godotcrypto@gmail.com";
+const appPassword = "mtnnchftufmuhmuh";
 
 router.post("/register", async (req, res) => {
     const { fname, email, password, referred } = req.body;
     const referCodet = shortid.generate();
     const referCode = referCodet.substring(0, 8);
-
-    console.log(fname, email, password, referred);
+    const otp = Math.floor(1000 + Math.random() * 9000);
 
     if (!fname || !email || !password) {
         res.status(422).json({ error: "Please Fill All Required Feilds" })
@@ -35,12 +36,33 @@ router.post("/register", async (req, res) => {
 
             const currentBalance = 10000;
 
-            const add = new user({ name: fname, email, password, referCode, currentBalance, referredBy });
+            const add = new user({ name: fname, email, password, referCode, currentBalance, referredBy, verifyCode: otp });
 
             const result = await add.save();
 
             if (result) {
-                res.status(200).json({ message: "User Registered" });
+                const transporter = mailer.createTransport({
+                    service: "gmail",
+                    auth: {
+                        user: senderMail,
+                        pass: appPassword
+                    }
+                });
+
+                const mailOption = {
+                    from: senderMail,
+                    to: email,
+                    subject: "Email Verification",
+                    html: `<h3> ${otp} is the OTP to verify your Email </h3>`
+                }
+
+                transporter.sendMail(mailOption, async (error, info) => {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        return res.status(200).json({ message: "Email Sent to your Email ID and User Registered" })
+                    }
+                })
             } else {
                 res.status(422).json({ error: "Error Occurred in Adding User" });
             }
@@ -48,8 +70,67 @@ router.post("/register", async (req, res) => {
     }
 })
 
-module.exports = router
+router.post("/verify", async (req, res) => {
+    const { email, code } = req.body;
+    const codeOtp = Number(code);
 
+
+    if (!email || !code) {
+        res.status(422).json({ error: "Please Fill All Required Feilds" })
+    } else {
+
+        const fetchUser = await user.findOne({ email: email });
+
+        if (fetchUser.verifyCode == codeOtp) {
+            const plans = await user.findOneAndUpdate({ email: email }, { $set: { verifyCode: 0 } });
+            return res.status(200).json({ message: "User verified!!! 🟢" })
+        } else {
+            res.status(422).json({ error: "Wrong OTP!!!" });
+        }
+
+    }
+})
+
+router.post("/resend", async (req, res) => {
+    const { email } = req.body;
+    const otp = Math.floor(1000 + Math.random() * 9000);
+
+    if (!email) {
+        res.status(422).json({ error: "Please Fill All Required Feilds" })
+    } else {
+
+        const plans = await user.findOneAndUpdate({ email: email }, { $set: { verifyCode: otp } });
+
+        if (plans) {
+            const transporter = mailer.createTransport({
+                service: "gmail",
+                auth: {
+                    user: senderMail,
+                    pass: appPassword
+                }
+            });
+
+            const mailOption = {
+                from: senderMail,
+                to: email,
+                subject: "Email Verification",
+                html: `<h3> ${otp} is the OTP to verify your Email </h3>`
+            }
+
+            transporter.sendMail(mailOption, async (error, info) => {
+                if (error) {
+                    console.log(error);
+                } else {
+                    return res.status(200).json({ message: "Email Sent to your Email ID Again" })
+                }
+            })
+        } else {
+            return res.status(422).json({ message: "Error sending email to your user again" })
+        }
+    }
+})
+
+module.exports = router
 
 
 // else if (referred == "") {
