172a062a1a96525d0171eee7d4b5badd2dd9d600
gotdot12 <129498729+gotdot12@users.noreply.github.com>
2023-05-28 22:57:48 +0530

    Update login.js

--- routers/login.js
+++ routers/login.js
@@ -6,14 +6,17 @@ const user = require("../models/user.model");
 router.post("/login", async (req, res) => {
     const { email, password } = req.body;
 
-
     if (!email || !password) {
         res.status(422).json({ error: "Please Fill All Required Feilds" })
     } else {
         const fetchUser = await user.findOne({ email: email });
 
         if (fetchUser.password == password) {
-            res.status(200).json({ error: "User Exist" });
+            if (fetchUser.verifyCode != 0) {
+                res.status(421).json({ error: "Verify Email" });
+            } else {
+                res.status(200).json({ error: "User Exist" });
+            }
         } else {
             res.status(422).json({ error: "User Not Exist" });
         }
